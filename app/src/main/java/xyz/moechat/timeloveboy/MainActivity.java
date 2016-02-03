5076d92047196bad334f15aa6126a015b4355611
timeloveboy <lipeng@mirrorsoft.cn>
2016-02-03 22:06:22 +0800

    给萌妹子打电话

---
+++ app/src/main/java/xyz/moechat/timeloveboy/MainActivity.java
@@ -0,0 +1,36 @@
+package xyz.moechat.timeloveboy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:18610423004"));
+        startActivity(intent);
+        Button launchbutton = (Button) findViewById(R.id.launchphone);
+        launchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18610423004"));
+                startActivity(intent);
+            }
+        });
+    }
+}
